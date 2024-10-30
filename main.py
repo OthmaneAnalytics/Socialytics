b5983183f91439ccf52c4608e62d65920e5e28a5
marbel <othmane.xx90@gmail.com>
2024-10-30 07:45:37 +0100

    recurrence on a tree

--- main.py
+++ main.py
@@ -1,40 +1,11 @@
-def find_minimum(nums):
-    minimum = float("inf")
-    if len(nums) == 0:
-        return None
-    for num in nums:
-        if num < minimum:
-            minimum = num
-    return minimum
-        
-def sum(nums):
-    tot = 0
-    for num in nums:
-        tot += num
-    return tot
+import functools
 
-def get_estimated_spread(audiences_followers):
-    num_followers = len(audiences_followers)
-    if num_followers == 0:
+def sum_nested_list(lst):
+    if not isinstance(lst,list):
+        return lst
+    elif len(lst) == 0:
         return 0
     else:
-        return sum(audiences_followers)/ num_followers * ( num_followers ** 1.2 )
+        return functools.reduce(lambda x, y : x + y , list(map(sum_nested_list,lst)))
+        
 
-def binary_search(target, arr):
-    begin = 0
-    end = len(arr) - 1
-    mid = (begin + end) //2
-    print(begin)
-    print(end)
-    print(mid)
-    while begin != end:
-        if end == begin and target != arr[mid]:
-            return None
-        elif target == arr[mid]:
-            return arr[mid]
-        elif target > arr[mid]:
-            begin = mid + 1
-            mid = (begin + mid) //2
-        else:
-            end = mid - 1
-                mid = (begin + mid) //2

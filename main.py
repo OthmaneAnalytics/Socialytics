67a51a8ad90b2e37611e60b37e401c0a9109953c
marbel <othmane.xx90@gmail.com>
2024-11-01 19:16:26 +0100

    quick sort

--- main.py
+++ main.py
@@ -212,3 +212,18 @@ def insertion_sort(nums):
             j -= 1
     return nums
 
+def quick_sort(nums, low, high):
+    if low < high:
+        i = partition(nums, low, high)
+        quick_sort(nums,low,i-1)
+        quick_sort(nums,i+1,high)
+
+def partition(nums, low, high):
+    pivot = nums[high]
+    i = low
+    for j in range(low, high):
+        if nums[j] < pivot:
+            nums[i], nums[j] = nums[j] , nums[i]
+            i += 1
+    nums[i], nums[high] = nums[high], nums[i]
+    return i

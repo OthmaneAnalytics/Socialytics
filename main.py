05089879fdaf76280ffeda78dfef4f2a3194c837
marbel <othmane.xx90@gmail.com>
2024-11-01 19:24:48 +0100

    reduction to P fibonacci

--- main.py
+++ main.py
@@ -227,3 +227,18 @@ def partition(nums, low, high):
             i += 1
     nums[i], nums[high] = nums[high], nums[i]
     return i
+
+def fib(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        f0 = 0
+        f1 = 1
+        f2 = 0
+        for i in range(2,n+1):
+            f2 = f1 + f0
+            f0 = f1
+            f1 = f2
+        return f2
